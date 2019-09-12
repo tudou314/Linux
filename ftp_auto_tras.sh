47267ffcbbc298ac72c94e673c077474763401e0
tudou314 <15155941679@139.com>
2019-09-12 09:30:48 +0800

    add ftp_auto.sh

---
+++ ftp_auto_tras.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#2018-05-08
+#ftp-auto-sh
+
+HOST='172.16.16.8'
+USER='waresoft'
+read -p 'Enter password: ' PASSWD
+#PASSWD='password'
+
+ftp -i -n $HOST <<EOF
+user ${USER} ${PASSWD}
+binary
+ls
+get xendesktop-server-ip.txt
+put auto_find_open_port.txt
+quit
+EOF
